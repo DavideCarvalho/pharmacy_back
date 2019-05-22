eecd00bda13c2794b2e18a635559eec92ee29281
Davi de Carvalho <davicarvalho96@yahoo.com.br>
2019-05-22 00:08:06 +0000

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import {HelloWorldModule} from './hello-world';
 import {PharmacyModule} from './pharmacy';
 import {MongooseModule} from '@nestjs/mongoose';
 
+const mongoAtlas = "mongodb+srv://root:root@develop-iex48.gcp.mongodb.net/test?retryWrites=true"
+const mongoUrl = `mongodb://${process.env.DATA_DB_HOST}:${process.env.DATA_DB_PORT}/${process.env.DATA_DB_NAME}`;
+
+
 @Module({
   imports: [
     MongooseModule.forRoot(
-      `mongodb://${process.env.DATA_DB_HOST}:${process.env.DATA_DB_PORT}/${process.env.DATA_DB_NAME}`,
+      mongoAtlas,
       {useNewUrlParser: true},
     ),
     HelloWorldModule,
