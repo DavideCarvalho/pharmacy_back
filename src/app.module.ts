d0136dc13378527fd8ec738f8de7195f85307be0
Davi de Carvalho <davi.carvalho@sysmap.com.br>
2019-05-22 09:14:43 -0300

    feat: fixing mongo url

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,10 @@ import {PharmacyModule} from './pharmacy';
 import { OauthModule } from './oauth';
 import {MongooseModule} from '@nestjs/mongoose';
 
-const mongoAtlas = "mongodb+srv://root:root@develop-iex48.gcp.mongodb.net/test?retryWrites=true"
-const mongoUrl = `mongodb://${process.env.DATA_DB_HOST}:${process.env.DATA_DB_PORT}/${process.env.DATA_DB_NAME}`;
-
-
 @Module({
   imports: [
     MongooseModule.forRoot(
-      `mongodb://${process.env.DATA_DB_HOST || '127.0.0.1'}:${process.env.DATA_DB_PORT || 27017}/${process.env.DATA_DB_NAME || 'pharmacy'}`,
+      `mongodb://${process.env.DATA_DB_HOST}:${process.env.DATA_DB_PORT}/${process.env.DATA_DB_NAME}`,
       {useNewUrlParser: true},
     ),
     HelloWorldModule,
