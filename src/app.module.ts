8320ab4ac4de3ef385d0f8cf1f3e4a404869028e
Davi de Carvalho <davi.carvalho@sysmap.com.br>
2019-05-22 08:01:29 -0300

    fix: merge conflict

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,10 @@ import {PharmacyModule} from './pharmacy';
 import { OauthModule } from './oauth';
 import {MongooseModule} from '@nestjs/mongoose';
 
+const mongoAtlas = "mongodb+srv://root:root@develop-iex48.gcp.mongodb.net/test?retryWrites=true"
+const mongoUrl = `mongodb://${process.env.DATA_DB_HOST}:${process.env.DATA_DB_PORT}/${process.env.DATA_DB_NAME}`;
+
+
 @Module({
   imports: [
     MongooseModule.forRoot(
