492f242b23f627ffb4734d24874456b512a5c3d1
Davi de Carvalho <davicarvalho96@yahoo.com.br>
2019-05-24 15:28:31 -0300

    feat: added swagger decorator on VO

--- src/pharmacy/vo/pharmacy.vo.ts
+++ src/pharmacy/vo/pharmacy.vo.ts
@@ -2,14 +2,19 @@ import {LocationVO} from './location.vo';
 import {ProductVO} from './product.vo';
 import {Type} from 'class-transformer';
 import {IsNotEmpty} from 'class-validator';
+import { ApiModelProperty, ApiModelPropertyOptional } from '@nestjs/swagger';
 
 export class PharmacyVO {
+  @ApiModelPropertyOptional()
   id: string;
+  @ApiModelProperty()
   @IsNotEmpty()
   name: string;
+  @ApiModelProperty()
   @Type(() => LocationVO)
   @IsNotEmpty()
   location: LocationVO;
+  @ApiModelProperty()
   @Type(() => ProductVO)
   products: ProductVO[];
 }
