346e02720c57e63a1d53d1910219bbc07aadb113
Davi de Carvalho <davicarvalho96@yahoo.com.br>
2019-05-25 14:13:41 -0300

    feat: fixing tests and pipes

--- src/commons/pipes/to-boolean.pipe.ts
+++ src/commons/pipes/to-boolean.pipe.ts
@@ -5,9 +5,6 @@ export class ToBooleanPipe implements PipeTransform {
     transform(value: any, metadata: ArgumentMetadata) {
         if (value === 'true') { return true; }
         if (value === 'false') { return false; }
-        throw new HttpException({
-            error: 'Parameter of wrong type, please send a boolean value',
-            status: HttpStatus.NOT_FOUND,
-        }, HttpStatus.NOT_FOUND);
+        if (!value) { return; }
     }
 }
--- src/commons/pipes/to-number.pipe.ts
+++ src/commons/pipes/to-number.pipe.ts
@@ -3,6 +3,7 @@ import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
 @Injectable()
 export class ToNumberPipe implements PipeTransform {
     transform(value: any, metadata: ArgumentMetadata) {
+        if (!value) { return; }
         return Number(value);
     }
 }
